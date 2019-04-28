c58de552c275c10156dd4a9243b69f4b621c0234
vinamraj <jvinamra776@gmail.com>
2019-04-28 09:18:31 +0530

    Initial application generated by JHipster-5.8.2

---
+++ src/main/java/com/vinamra/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.vinamra.service.dto;
---
+++ src/main/java/com/vinamra/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.vinamra.web.rest.vm;
